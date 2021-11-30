8ce979bc3d7262d4ed2a6c9b9185ac7c6e7c21b4
sabs21 <sabian@my.ccsu.edu>
2021-11-29 23:56:32 -0500

    created skeletons for sorting algorithm classes

---
+++ lib/src/main/java/com/group3/racingbot/sorting/DriverStandingsMergeSort.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.group3.racingbot.sorting;
+
+import java.util.List;
+
+import com.group3.racingbot.standings.DriverStanding;
+
+/**
+ * Sorts a given list of DriverStandings using the merge sort algorithm.
+ * @author Nick Sabia
+ */
+public class DriverStandingsMergeSort implements SortStandings {
+
+	@Override
+	public List<DriverStanding> sortByTimeCompleted() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<DriverStanding> sortByDistanceTraveled() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ lib/src/main/java/com/group3/racingbot/sorting/DriverStandingsQuickSort.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.group3.racingbot.sorting;
+
+import java.util.List;
+
+import com.group3.racingbot.standings.DriverStanding;
+
+/**
+ * Sorts a given list of DriverStandings using the quick sort algorithm.
+ * @author Nick Sabia
+ */
+public class DriverStandingsQuickSort implements SortStandings {
+
+	@Override
+	public List<DriverStanding> sortByTimeCompleted() {
+		
+		return null;
+	}
+
+	@Override
+	public List<DriverStanding> sortByDistanceTraveled() {
+		
+		return null;
+	}
+	
+}
