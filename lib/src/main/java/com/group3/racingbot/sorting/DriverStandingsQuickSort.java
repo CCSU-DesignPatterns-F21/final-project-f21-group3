320b84c43ac5f03efbb045820f77b34fe6d750fe
sabs21 <sabian@my.ccsu.edu>
2021-11-30 00:14:10 -0500

    added quicksort algorithm

--- lib/src/main/java/com/group3/racingbot/sorting/DriverStandingsQuickSort.java
+++ lib/src/main/java/com/group3/racingbot/sorting/DriverStandingsQuickSort.java
@@ -15,14 +15,59 @@ public class DriverStandingsQuickSort implements SortStandings {
 
 	@Override
 	public List<DriverStanding> sortByTimeCompleted(List<DriverStanding> list) {
-		
+		quicksortByTimeCompleted(list, 0, list.size()-1);
 		return null;
 	}
 
 	@Override
 	public List<DriverStanding> sortByDistanceTraveled(List<DriverStanding> list) {
-		
+		quicksortByDistance(list, 0, list.size()-1);
 		return null;
 	}
 	
+	private static void quicksortByTimeCompleted(List<DriverStanding> list, int low, int high){
+	    if(low < high){
+	        int p = partitionByTimeCompleted(list, low, high);
+	        quicksortByTimeCompleted(list, low, p-1);
+	        quicksortByTimeCompleted(list, p+1, high);
+	    }
+	}
+	
+	private static int partitionByTimeCompleted(List<DriverStanding> list, int low, int high){
+	    int p = low, j;
+	    for(j=low+1; j <= high; j++) {
+	    	if (list.get(j).getTimeCompleted() < list.get(low).getTimeCompleted()) {
+	        	swap(list, ++p, j);
+	        }
+	    }
+	    
+	    swap(list, low, p);
+	    return p;
+	}
+	
+	private static void quicksortByDistance(List<DriverStanding> list, int low, int high){
+	    if(low < high){
+	        int p = partitionByDistance(list, low, high);
+	        quicksortByDistance(list, low, p-1);
+	        quicksortByDistance(list, p+1, high);
+	    }
+	}
+	
+	private static int partitionByDistance(List<DriverStanding> list, int low, int high){
+	    int p = low, j;
+	    for(j=low+1; j <= high; j++) {
+	    	if (list.get(j).getDistanceTraveled() < list.get(low).getDistanceTraveled()) {
+	        	swap(list, ++p, j);
+	        }
+	    }
+	    
+	    swap(list, low, p);
+	    return p;
+	}
+	
+	private static void swap(List<DriverStanding> list, int a, int b){
+		DriverStanding tmp = list.get(a);
+		list.set(a, list.get(b));
+		list.set(b, tmp);
+	}
 }
